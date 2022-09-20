ea3f103f3a7f5da97147f3fd4daca4256320daf0
diana-bronitskaya <dianalyla47@gmail.com>
2022-09-20 20:54:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let value1 = '';
+let value2 = '';
+let sign = '';
+
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+let operation = ['+', '-', 'X', '/', '+/-', '%'];
+
+const output = document.querySelector('input'); 
+
+let btn = document.querySelector('.numbers');
+
+// document.querySelector('.AC').onclick = ClearAll;
+// let x = document.querySelector(".AC");
+// x.addEventListener('click', ClearAll);
+
+document.querySelector(".AC").addEventListener("click", ClearAll);
+function ClearAll(e) {
+    console.log('e.target.innerText');
+}
+
+console.log(value1);
+
+
+
+
